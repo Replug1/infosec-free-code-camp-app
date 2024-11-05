b921bb952e22d574669a65aabcbf4708358c72b8
Replug1 <lucasleonel001@gmail.com>
2024-11-04 22:12:06 -0300

    Add noSniff for MIME protection

--- myApp.js
+++ myApp.js
@@ -4,8 +4,8 @@ const app = express();
 
 app.use(helmet.hidePoweredBy());
 app.use(helmet.frameguard({action: 'deny'}));
-app.use(helmet.xssFilter())
-
+app.use(helmet.xssFilter());
+app.use(helmet.noSniff());
 
 
 
